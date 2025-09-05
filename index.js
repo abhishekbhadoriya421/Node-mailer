cddf23d8091fbae6c0509b889f2c863a4c10ee53
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-05 08:52:52 +0530

    Install dependancies

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+const port = 8080;
+
+
+app.get('/', (req, res) => {
+    return res.s
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
+
